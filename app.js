48319ee6e73056645544fc3fc72947dbf3b69f26
ihtshamkhan <ihtshamkhan88@gmail.com>
2024-03-11 14:58:19 +0500

    js tutorial

---
+++ app.js
@@ -0,0 +1,13 @@
+let shopName = prompt("which shop is open?");
+if (shopName === "farhan") {
+    console.log("bring french fries");
+}
+else if (shopName === "almadina") {
+    console.log("bring ande wala burger");
+}
+else if (shopName === "ijaz") {
+    console.log("bring buryani");
+}
+else{
+    console.log("bring something to eat");
+}
